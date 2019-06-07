be4d0f660e00cdffc594d16af0042977c2f26754
d_mokharev <d_mokharev@todes.by>
2019-06-07 10:34:38 +0300

    Start

---
+++ StudyJava/src/main/java/ordinal/snail/java/Main.java
@@ -0,0 +1,16 @@
+package ordinal.snail.java;
+
+/**
+ *
+ * @author d_mokharev
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
---
+++ StudyJava/src/main/java/ordinal/snail/java/collections/Basics.java
@@ -0,0 +1,18 @@
+package ordinal.snail.java.collections;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author d_mokharev
+ */
+public class Basics {
+
+    private ArrayList<Integer> arrList = new ArrayList<>();
+
+
+
+    public Basics() {
+    }
+
+}
