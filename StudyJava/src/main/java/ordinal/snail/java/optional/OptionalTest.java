4ae1b30fbd65478be59d61aa26608d4861a94c3b
d_mokharev <d_mokharev@todes.by>
2019-07-04 16:27:21 +0300

    study optional

--- StudyJava/src/main/java/ordinal/snail/java/optional/OptionalTest.java
+++ StudyJava/src/main/java/ordinal/snail/java/optional/OptionalTest.java
@@ -1,6 +1,7 @@
 package ordinal.snail.java.optional;
 
 import java.util.Optional;
+import ordinal.snail.java.collections.list.ListTest;
 
 /**
  *
@@ -9,10 +10,24 @@ import java.util.Optional;
 public class OptionalTest {
 
     public static void go() throws Exception {
-        Optional<String> fullName = Optional.ofNullable(null);
-        System.out.println("Full Name is set? " + fullName.isPresent());
-        System.out.println("Full Name: " + fullName.orElseGet(() -> "[none]"));
-        System.out.println(fullName.map(s -> "Hey " + s + "!").orElse("Hey Stranger!"));
+        System.err.println("test null: ");
+        String str = null;
+        Optional<String> person = Optional.of(str);
+        person.ifPresent(System.out::println);
+
+        System.err.println("test empty: ");
+        String str2 = null;
+        Optional<String> person2 = Optional.of(str2);
+        person2.ifPresent(System.out::println);
+
+
+
+
+//        Optional<String> fullName = Optional.ofNullable(null);
+//        System.out.println("Full Name is set? " + fullName.isPresent());
+//        System.out.println("Full Name: " + fullName.orElseGet(() -> "[none]"));
+//        System.out.println(fullName.map(s -> "Hey " + s + "!").orElse("Hey Stranger!"));
+
     }
 
 }
